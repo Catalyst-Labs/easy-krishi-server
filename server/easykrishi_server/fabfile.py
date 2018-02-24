ccaf0d7a547b572e914940189b803091ffa71055
Varun Bhat <varunbhat.kn@gmail.com>
2018-02-24 01:19:45 -0700

    Beautify and added dev host(localhost)

--- server/easykrishi_server/fabfile.py
+++ server/easykrishi_server/fabfile.py
@@ -14,28 +14,32 @@ from fabric.contrib import files
 from fabric import state
 from fabric import network
 import pickle
+import os
 
+if not os.environ.get('DEV', False):
+	env.hosts = ['ec2-54-144-105-167.compute-1.amazonaws.com']
+else:	
+	env.hosts = ['localhost']
 
-env.hosts = ['ec2-54-144-105-167.compute-1.amazonaws.com']
 env.key_filename = "/home/ubuntu/public.pem"
 def deploy(user,password,db):
-		shared = {"DB_NAME":db, "DB_USER":user,"DB_PASSWORD":password}
-		fp = open("shared.obj","wb")
-		pickle.dump(shared, fp)
-		fp.close()
+	shared = {"DB_NAME":db, "DB_USER":user,"DB_PASSWORD":password}
+	fp = open("shared.obj","wb")
+	pickle.dump(shared, fp)
+	fp.close()
 
-		my_site(user,password,db)
-		with cd('.'):
-				#local('pkill gunicorn')
-				local('python manage.py makemigrations')
-				local('python manage.py migrate')
-				local('gunicorn easykrishi.wsgi:application --bind=0.0.0.0:8000 --workers=4 --daemon')
-def my_site(user,password,db):
-		env.hosts = ['ec2-54-144-105-167.compute-1.amazonaws.com']
-		env.key_filename = "/home/ubuntu/public.pem"
-		env.conf = dict(
-				DB_USER = user,
-				DB_PASSWORD = password,
-				DB_NAME = db
-		)
-		#update_env()
+	my_site(user,password,db)
+	with cd('.'):
+		#local('pkill gunicorn')
+		local('python manage.py makemigrations')
+		local('python manage.py migrate')
+		local('gunicorn easykrishi.wsgi:application --bind=0.0.0.0:8000 --workers=4 --daemon')
+
+def my_site(user,password,db):	
+	env.key_filename = "/home/ubuntu/public.pem"
+	env.conf = dict(
+			DB_USER = user,
+			DB_PASSWORD = password,
+			DB_NAME = db
+	)
+	#update_env()
